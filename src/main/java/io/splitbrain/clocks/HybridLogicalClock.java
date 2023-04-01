c021669cee09bb6ea8bc2934c947abc4508989d6
Hugo Firth <me@hugofirth.com>
2023-04-01 11:22:42 +0100

    Wrote tick function for HLC

--- src/main/java/io/splitbrain/clocks/HybridLogicalClock.java
+++ src/main/java/io/splitbrain/clocks/HybridLogicalClock.java
@@ -3,6 +3,7 @@ package io.splitbrain.clocks;
 import io.splitbrain.util.PartialComparison;
 
 import java.time.Clock;
+import java.time.Duration;
 import java.time.Instant;
 
 /**
@@ -10,7 +11,7 @@ import java.time.Instant;
  * "Logical Physical Clocks and Consistent Snapshots in Globally Distributed Databases".
  * <p>
  * Hybrid logical clocks are based on standard wall clock time (e.g. that provided by
- * `System.currentTimeMillis`), but use an additional counter in order to ensure monotonicity.
+ * {@link System#currentTimeMillis()}), but use an additional counter in order to ensure monotonicity.
  * <p>
  * They improve upon other logical clocks (such as Lamport clocks) because they offer minimal
  * drift from their underlying wall clock over time. I.e. a single timestamp value may be used
@@ -39,27 +40,48 @@ public class HybridLogicalClock implements PhysicalClock {
 
     @Override
     public PhysicalClock tick() {
-        return null;
+        var tickedClock = physicalClock.tick();
+        var physicalTimeNow = tickedClock.getTimestamp();
+        var previousTimestamp = timestamp;
+        var nextTimestamp = laterInstant(physicalTimeNow, previousTimestamp);
+        var nextCounter = nextTimestamp.equals(previousTimestamp) ?  counter + 1 : 0;
+
+        return new HybridLogicalClock(tickedClock, nextTimestamp, nextCounter);
     }
 
     @Override
     public Instant getTimestamp() {
-        return null;
+        return physicalClock.getTimestamp();
     }
 
     @Override
     public PhysicalClock startOfTime() {
-        physicalClock.startOfTime();
-        return null;
+        return physicalClock.startOfTime();
     }
 
+    //TODO: Make generic so merge is in terms of HLC
     @Override
     public PhysicalClock merge(PhysicalClock l, PhysicalClock r) {
-        return null;
+        var previousTimestampL = l.getTimestamp();
+        var previousTimestampR = r.getTimestamp();
+        var nextTimestamp = laterInstant(previousTimestampL, previousTimestampR);
+        var nextCounter = 0;
+        if (previousTimestampL.equals(previousTimestampR)) {
+            Math.max(l.counter)
+        }
+        return ;
     }
 
     @Override
     public PartialComparison compareTo(PhysicalClock other) {
         return null;
+    }
+
+    private Instant laterInstant(Instant a, Instant b) {
+        return a.isAfter(b) ? a : b;
+    }
+
+    private int counter() {
+        return counter;
     }
 }
